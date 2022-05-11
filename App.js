a76abc37dbd03c1a1fa717a8ddf334d7929b316a
Pablo Peña <insomnionebulous@gmail.com>
2022-05-11 19:38:27 -0300

    avance estilos

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { useState } from "react"
 import { StatusBar } from "expo-status-bar"
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Alert,
-  Dimensions,
-  TextInput,
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableOpacity,
+	Alert,
+	Dimensions,
+	TextInput,
 } from "react-native"
 import Constants from "expo-constants"
 import MorningBackground from "./assets/images/backgrounds/morning.png"
+import Icon from "./assets/images/icons/clouds.png"
 import NoonBackground from "./assets/images/backgrounds/noon.png"
 import NightBackground from "./assets/images/backgrounds/night.png"
 import MidightBackground from "./assets/images/backgrounds/midnight.png"
@@ -22,85 +23,83 @@ import { useGetCurrentWeather } from "./hooks/useGetCurrentWeather"
 
 const win = Dimensions.get("window")
 export default function App() {
-  /*   const weather = useGetCurrentWeather()
-   */ let hours = new Date().getHours()
-  let background = null
-  if (hours >= 6 && hours < 12) {
-    background = MorningBackground
-  } else if (hours >= 12 && hours < 18) {
-    background = NoonBackground
-  } else {
-    background = NightBackground
-  }
-  /*   weather && console.log(weather)
-   */
+	/*   const weather = useGetCurrentWeather()
+	 */
+	let hours = new Date().getHours()
+	let background = null
+	if (hours >= 6 && hours < 12) {
+		background = MorningBackground
+	} else if (hours >= 12 && hours < 18) {
+		background = NoonBackground
+	} else {
+		background = NightBackground
+	}
+	/*   weather && console.log(weather)
+	 */
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style={hours < 6 ? "light" : "dark"} />
-      <View style={styles.backgroundCtn}>
-        <Image source={background} style={styles.background} />
-      </View>
-      <View style={styles.content}>
-        {1 ? (
-          <Image
-            source={{
-              uri: "http://openweathermap.org/img/wn/04n@2x.png",
-              /* 							uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
-               */
-            }}
-            style={styles.icon}
-          />
-        ) : null}
-      </View>
-      <TextInput
-        placeholder="Enter your city"
-        style={styles.input}
-        onChangeText={(e) => {
-          console.log(e)
-        }}
-      />
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<View style={styles.backgroundCtn}>
+				<Image source={background} style={styles.background} />
+			</View>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "center",
+					alignItems: "center",
+					paddingTop: 150,
+				}}
+			>
+				<Image
+					source={Icon}
+					style={{ height: 75, width: 75, marginRight: 10 }}
+				/>
+				<Text style={styles.degrees}>20°</Text>
+			</View>
+			<TextInput
+				placeholder="Enter your city"
+				style={styles.input}
+				onChangeText={(e) => {}}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: "relative",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  backgroundCtn: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: win.width,
-    height: win.height + Constants.statusBarHeight,
-  },
-  background: {
-    height: "100%",
-    width: "100%",
-    resizeMode: "cover",
-  },
-  input: {
-    backgroundColor: "#fff",
-    width: "80%",
-    height: 50,
-    paddingStart: 20,
-    borderRadius: 10,
-  },
-  content: {
-    width: "80%",
-    padding: 20,
-    margin: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  icon: {
-    width: 100,
-    height: 100,
-    backgroundColor: "grey",
-  },
+	container: {
+		flex: 1,
+		position: "relative",
+		alignItems: "center",
+	},
+	backgroundCtn: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: win.width,
+		height: win.height,
+	},
+	background: {
+		height: "100%",
+		width: "100%",
+		resizeMode: "cover",
+		marginTop: Constants.statusBarHeight,
+	},
+	input: {
+		backgroundColor: "#fff",
+		width: "80%",
+		height: 50,
+		paddingStart: 20,
+		borderRadius: 10,
+	},
+	degrees: {
+		fontSize: 100,
+		color: "#fff",
+		fontFamily: "sans-serif",
+	},
+	icon: {
+		width: 100,
+		height: 100,
+		backgroundColor: "grey",
+	},
 })
