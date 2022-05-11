634f848db27c923f612f23fead9991c16632cc38
Pablo Peña <insomnionebulous@gmail.com>
2022-05-11 11:49:49 -0300

    avance

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { StatusBar } from "expo-status-bar"
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableOpacity,
+	Alert,
+	Dimensions,
+	TextInput,
+} from "react-native"
+import Constants from "expo-constants"
+import MorningBackground from "./assets/images/morning.png"
+import NoonBackground from "./assets/images/noon.png"
+import NightBackground from "./assets/images/night.png"
+import MidightBackground from "./assets/images/midnight.png"
+import LateBackground from "./assets/images/late.png"
+import MornignEarlyBackground from "./assets/images/morning-early.png"
+import MorningDarkBackground from "./assets/images/morning-dark.png"
+import { useGetCurrentWeather } from "./hooks/useGetCurrentWeather"
 
+const win = Dimensions.get("window")
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	/* 	const weather = useGetCurrentWeather()
+	 */ let hours = new Date().getHours() //To get the Current Hours
+	let background = null
+	if (hours >= 6 && hours < 12) {
+		background = MorningBackground
+	} else if (hours >= 12 && hours < 18) {
+		background = NoonBackground
+	} else {
+		background = NightBackground
+	}
+	/* 	weather && console.log(weather.weather[0].description)
+	
+	 */
+	let weather = 1
+	return (
+		<View style={styles.container}>
+			<StatusBar style={hours < 6 ? "light" : "dark"} />
+			<View style={styles.backgroundCtn}>
+				<Image source={background} style={styles.background} />
+			</View>
+			<View style={styles.content}>
+				{weather ? (
+					<Image
+						source={{
+							uri: "http://openweathermap.org/img/wn/04n@2x.png",
+							/* 							uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+							 */
+						}}
+						style={styles.icon}
+					/>
+				) : null}
+			</View>
+			<TextInput
+				placeholder="Enter your city"
+				style={styles.input}
+				onChangeText={(e) => {
+					console.log(e)
+				}}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		position: "relative",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	backgroundCtn: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: win.width,
+		height: win.height + Constants.statusBarHeight,
+	},
+	background: {
+		height: "100%",
+		width: "100%",
+		resizeMode: "cover",
+	},
+	input: {
+		backgroundColor: "#fff",
+		width: "80%",
+		height: 50,
+		paddingStart: 20,
+		borderRadius: 10,
+	},
+	content: {},
+	icon: {
+		width: 100,
+		height: 100,
+		backgroundColor: "grey",
+	},
+})
---
+++ hooks/useGetCurrentWeather.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+
+export const useGetCurrentWeather = () => {
+	const [weather, setWeather] = useState(null)
+
+	useEffect(() => {
+		fetch(
+			"https://api.openweathermap.org/data/2.5/weather?q=Montevideo&appid=b5ba6873c874531bc8e0b376c9253158"
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				setWeather(data)
+			})
+	}, [])
+	return weather
+}
